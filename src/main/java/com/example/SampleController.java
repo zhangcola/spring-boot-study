b1472f39bddb11aab2e8e9608f07df6436c80ec0
cola <zhangzhenti@163.com>
2017-01-22 21:51:48 +0800

    Initial commit

---
+++ src/main/java/com/example/SampleController.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by cola on 2017/1/18.
+ */
+@SpringBootApplication
+@RestController
+public class SampleController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "Hello spring boot";
+    }
+}
